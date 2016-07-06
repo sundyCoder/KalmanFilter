11500cf72178649730fc65b23980c1ed345adddf
sundy <sundycoder@gmail.com>
2016-07-06 21:27:43 +0800

    1.implement the ekf algorithm on robot.

--- code/matlab/ekf_robot/plot_robot.m
+++ code/matlab/ekf_robot/plot_robot.m
@@ -1,37 +1,44 @@
+
 clear all
 clc;
-close all;
+% close all;
 
-mode = 1;
+mode = 2;
 
+% Robot length
+s.L = 0.0612;
 % Robot wheel length
-s.L = 0.612;
 s.W = 0.0195;
+
+% The height of beacon.
+S.Height = 0.40;
+
 % Position of Beacon1
 s.B1 = [0, 0];                  
 % Position of Beacon2
 s.B2 = [0, 0.54];
 
-s.x = [1.2;0.13;0.5];
+%s.x = [ 0.4251; 0.0639; 6.2632]; % test2
+s.x = [ 0.6571; 0.1215;  6.0039];  % test3
 % State transition matrix
 s.A = [1, 0, 0.1;
        0, 1, 0.1;
        0, 0, 1];
 % Define a process noise:
-s.Q = 1e-10*eye(3);
+s.Q = 1e-3*eye(3);
 
 % Observation Matrix
 s.H = [0.1, 0.1,0;
        0.1, 0.1,0;
        0,0, 1];
 % Define a measurement error :
-s.R = 10*[1,0,0;
+s.R = 1e-1*[1,0,0;
        0,1,0;
        0,0,0.1];
    
 % Do not define any system input (control) functions:
-s.B = 0;
-s.u = 0;
+% s.B = 0;
+% s.u = 0;
 % Do not specify an initial state:
 s.P = 10e6*eye(3);
 
@@ -40,15 +47,15 @@ s.P = 10e6*eye(3);
 %     {'*.txt', '*.*'}, ...
 %     'Pick a file');
 
-data = load('log.txt');
+data = load('test3.txt');
 [row, column] = size(data);
 
 % Calibrate north angle paraell with x axis
 for i = 1:row
-    if (data(i,4) > 115) && (data(i,4) <= 180)    
-        data(i,4) = data(i,4) - 115;        
+    if (data(i,4) >= 70) && (data(i,4) < 180)    
+        data(i,4) = data(i,4) - 70;        
     else
-        data(i,4) = data(i,4) + 245;
+        data(i,4) = data(i,4) + 290;
     end       
 end
 
@@ -59,7 +66,7 @@ data(:,5:6) = (data(:,5:6)/512)*(1/28)*(12/30)*pi*s.W;
 if mode == 1
     data(:,2:3) = data(:,2:3).^2;
 elseif mode ==2
-    data(:,2:3) = data(:,2:3).^2  - 0.40.^2;
+    data(:,2:3) = data(:,2:3).^2  - S.Height.^2;
 end
 
 % + 0.01*randn(size(data(:,2:3))); %distance noise
@@ -74,12 +81,11 @@ if mode == 1
     for i=1:row
        y = (data(i,2) - data(i,3) + 0.54.^2)/(2*0.54);
        x = sqrt(data(i,2) - 0.54.^2 - y.^2);    
-    %    %x = abs(x);
-    %    if ~isreal(x)
-    %       aa=[aa,i];
-    %    end
+%        if ~isreal(x)
+%           aa=[aa,i];
+%        end
        plot(x,y,'o');
-       %pause(0.01);
+       pause(0.01);
        hold on;
     end
 elseif mode == 2     
@@ -89,10 +95,13 @@ elseif mode == 2
         X = kalmanf_robot(s);
         x = X.x(1); y = X.x(2);
         plot(x,y,'-o');
-        %pause(0.01);
+       % pause(0.1);
         hold on;
     end
 end
-
-% data = load('log1.txt');
+% 
+% data = load('test3.txt');
 % data(aa,:)=[];
+% fileID = fopen('test3.txt','w');
+% fprintf(fileID,'%f %f %f %f %f %f\n',data');
+
